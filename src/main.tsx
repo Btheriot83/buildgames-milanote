e14c36bbbd285f38b1ed787708bad3170238249c
Brandon Theriot <Btheriot83@users.noreply.github.com>
2026-09-15 05:08:45 +0000

    r1: fonts

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import './styles/transitions.css'
 import './styles/flat-bar.css'
 import './styles/b3-craft.css'
+import './styles/b4-integrity.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
